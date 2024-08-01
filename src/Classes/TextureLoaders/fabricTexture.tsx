acb3866366369a6fd295175d85ed7d897479612f
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2024-08-01 18:59:59 +0300

    forgot to save oops

--- src/Classes/TextureLoaders/fabricTexture.tsx
+++ src/Classes/TextureLoaders/fabricTexture.tsx
@@ -32,7 +32,7 @@ const fabricTexture = (
     map: colorMap,
     aoMap: armMap,
     displacementMap: displacementMap,
-    displacementScale: 0,
+    displacementScale: 0.0003,
     normalMap: normalDXMap,
     roughnessMap: armMap,
     metalnessMap: armMap,
